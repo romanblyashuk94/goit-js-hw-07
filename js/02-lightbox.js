55f06b0d747504803c46c58529d0c4311100b37c
Roman Bliashuk <romanblyashuk94@gmail.com>
2022-05-11 10:57:17 +0300

    2nd task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+galleryRef.innerHTML = createGalleryImagesMurkup(galleryItems);
+
+function createGalleryImagesMurkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            data-source=${original}
+            alt=${description}
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
