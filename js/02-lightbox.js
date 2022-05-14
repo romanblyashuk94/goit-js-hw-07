2ddc44d0734bbcc853dccb69b4863716d779a734
Roman Bliashuk <romanblyashuk94@gmail.com>
2022-05-15 01:30:36 +0300

    remove all changes with parcel

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,9 +1,25 @@
 import { galleryItems } from "./gallery-items.js";
-import createGalleryImagesMurkup from './galleryFunctions/createGalleryImagesMurkup'
-import SimpleLightbox from "simplelightbox"
 // Change code below this line
 
 const galleryRef = document.querySelector(".gallery");
 galleryRef.innerHTML = createGalleryImagesMurkup(galleryItems);
 
-var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+var lightbox = new SimpleLightbox(".gallery a");
+
+function createGalleryImagesMurkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href=${original}>
+            <img
+              class="gallery__image"
+              src=${preview}
+              data-source=${original}
+              alt=${description}
+            />
+          </a>
+        </div>`;
+    })
+    .join("");
+}
