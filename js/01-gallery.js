f1f7fc303b963317e227b9bcf455352419bdac57
Roman Bliashuk <romanblyashuk94@gmail.com>
2022-05-16 16:22:15 +0300

    deploing project to gh-pages

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,75 +1,13 @@
 import { galleryItems } from "./gallery-items.js";
+import createGalleryImagesMurkup from "./galleryFunctions/createGalleryImagesMurkup.js";
+import { onOpenImageModal } from "./galleryFunctions/eventListenersCallbacks.js";
+
 // Change code below this line
 
 // Refs:
 const gallery = document.querySelector(".gallery");
-const selectedImageModal = document.querySelector(".backdrop");
-const selectedImage = selectedImageModal.querySelector(
-  ".selectedImageModal__image"
-);
-const closeBtn = document.querySelector(".closeButton");
 // -------------------------------------------
 
 const galleryImagesMurkup = createGalleryImagesMurkup(galleryItems);
 gallery.insertAdjacentHTML("beforeend", galleryImagesMurkup);
 gallery.addEventListener("click", onOpenImageModal);
-
-function onOpenImageModal(e) {
-  if (!e.target.classList.contains("gallery__image")) {
-    return;
-  }
-
-  e.preventDefault();
-  document.body.classList.add("modal-open");
-  selectedImage.src = e.target.dataset.source;
-  selectedImage.alt = e.target.alt;
-
-  addEventListenersToCloseElements();
-}
-
-function onCloseImageModal(e) {
-  if (
-    !e.target.classList.contains("backdrop") &&
-    !e.target.classList.contains("closeButton") &&
-    !(e.code === "Escape")
-  ) {
-    return;
-  }
-
-  document.body.classList.remove("modal-open");
-
-  selectedImage.src = "";
-  selectedImage.alt = "";
-
-  removeEventListenersFromCloseElements();
-}
-
-function addEventListenersToCloseElements() {
-  closeBtn.addEventListener("click", onCloseImageModal);
-  selectedImageModal.addEventListener("click", onCloseImageModal);
-  window.addEventListener("keydown", onCloseImageModal);
-}
-
-function removeEventListenersFromCloseElements() {
-  closeBtn.removeEventListener("click", onCloseImageModal);
-  selectedImageModal.removeEventListener("click", onCloseImageModal);
-  window.removeEventListener("keydown", onCloseImageModal);
-}
-
-function createGalleryImagesMurkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-    <div class="gallery__item">
-      <a class="gallery__link" href=${original}>
-        <img
-          class="gallery__image"
-          src=${preview}
-          data-source=${original}
-          alt=${description}
-        />
-      </a>
-    </div>`;
-    })
-    .join("");
-}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,25 +1,9 @@
 import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import createGalleryImagesMurkup from './galleryFunctions/createGalleryImagesMurkup'
 // Change code below this line
 
 const galleryRef = document.querySelector(".gallery");
 galleryRef.innerHTML = createGalleryImagesMurkup(galleryItems);
 
 var lightbox = new SimpleLightbox(".gallery a");
-
-function createGalleryImagesMurkup(galleryItems) {
-  return galleryItems
-    .map(({ preview, original, description }) => {
-      return `
-        <div class="gallery__item">
-          <a class="gallery__link" href=${original}>
-            <img
-              class="gallery__image"
-              src=${preview}
-              data-source=${original}
-              alt=${description}
-            />
-          </a>
-        </div>`;
-    })
-    .join("");
-}
